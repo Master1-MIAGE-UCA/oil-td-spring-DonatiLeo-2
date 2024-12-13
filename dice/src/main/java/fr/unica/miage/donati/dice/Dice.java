9ade82a3d7b608da0a1278c58c551747aa54eb99
Leo Donati <leo.donati@unice.fr>
2024-12-13 09:38:27 +0100

    Make Dice a Bean

--- dice/src/main/java/fr/unica/miage/donati/dice/Dice.java
+++ dice/src/main/java/fr/unica/miage/donati/dice/Dice.java
@@ -1,5 +1,8 @@
 package fr.unica.miage.donati.dice;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dice {
     public int roll() {
         return (int) (Math.random() * 6) + 1;
